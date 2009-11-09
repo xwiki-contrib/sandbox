ca7a8465281cedca0e73531aa4fa538e41832c60
Vincent Massol <vincent@massol.net>
2009-11-09 15:13:45 +0000

    Moved to the new contrib repo
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/contrib/sandbox@24953 f329d543-caf0-0310-9063-dda96c69346f

---
+++ importexport/xwiki-core-importer/src/main/java/org/xwiki/wikiimporter/wiki/WikiPageRevision.java
@@ -0,0 +1,63 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.wikiimporter.wiki;
+
+import java.util.List;
+
+/**
+ * WikiPageRevision interface represents a Wiki Page Revision used during import process.
+ * 
+ * @version $Id$
+ */
+public interface WikiPageRevision {
+
+
+	/**
+	 *@return the author of Page revision.
+	 */
+	public String getAuthor();
+	
+
+	/**
+	 *@return the list of attachments of a Page revision.
+	 */
+	public List<Attachment> getAttachments();
+	
+	/**
+	 *@return the version of Page revision.
+	 */
+	public String getVersion();
+	
+	/**
+	 *@return the text content of Page revision.
+	 */
+	public String getContent();
+	
+	/**
+	 *@return the comment on Page revision.
+	 */
+	public String getComment();
+	
+	/**
+	 *@return <tt>true</tt> if the revision is  a minor edit, <tt>false</tt> if not.
+	 */
+	public boolean isMinorEdit();
+	
+}
