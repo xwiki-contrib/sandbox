578140c38e56ec969709f8f366e0daf6a4f81b47
Anca Paula Luca <lucaa@xwiki.com>
2009-11-17 22:22:39 +0000

    Removed unused import.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/contrib/sandbox@25104 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-annotation-parent/xwiki-annotation-io/src/main/java/org/xwiki/annotation/internal/maintainment/AbstractAnnotationMaintainer.java
+++ xwiki-annotation-parent/xwiki-annotation-io/src/main/java/org/xwiki/annotation/internal/maintainment/AbstractAnnotationMaintainer.java
@@ -23,7 +23,6 @@ package org.xwiki.annotation.internal.maintainment;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import org.xwiki.annotation.AnnotationMaintainer;
