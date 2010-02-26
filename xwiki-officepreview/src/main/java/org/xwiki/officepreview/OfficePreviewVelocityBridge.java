063bfdf4d6c444ef493b75560ba4c16dab6cac23
Asiri Rathnayake <asiri@xwiki.com>
2010-02-26 02:59:21 +0000

    * Minor code improvement.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/contrib/sandbox@27298 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-officepreview/src/main/java/org/xwiki/officepreview/OfficePreviewVelocityBridge.java
+++ xwiki-officepreview/src/main/java/org/xwiki/officepreview/OfficePreviewVelocityBridge.java
@@ -28,7 +28,7 @@ import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.context.Execution;
 import org.xwiki.model.reference.AttachmentReference;
 import org.xwiki.model.reference.AttachmentReferenceResolver;
-import org.xwiki.model.reference.EntityReferenceSerializer;
+import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.rendering.block.Block;
 import org.xwiki.rendering.block.XDOM;
 import org.xwiki.rendering.renderer.BlockRenderer;
@@ -88,11 +88,6 @@ public class OfficePreviewVelocityBridge
     private DocumentAccessBridge docBridge;
 
     /**
-     * Used to serialize {@link EntityReference} instances into strings.
-     */
-    private EntityReferenceSerializer<String> refSerializer;
-
-    /**
      * Constructs a new bridge instance.
      * 
      * @param componentManager used to lookup for other required components.
@@ -112,7 +107,6 @@ public class OfficePreviewVelocityBridge
         this.defaultOfficePreviewBuilder = componentManager.lookup(OfficePreviewBuilder.class);
         this.presentationOfficePreviewBuilder = componentManager.lookup(OfficePreviewBuilder.class, "presentation");
         this.docBridge = componentManager.lookup(DocumentAccessBridge.class);
-        this.refSerializer = componentManager.lookup(EntityReferenceSerializer.class);
     }
 
     /**
@@ -161,15 +155,15 @@ public class OfficePreviewVelocityBridge
      */
     private String preview(AttachmentReference attachRef, String outputSyntaxId) throws Exception
     {
-        String strDocumentName = refSerializer.serialize(attachRef.getDocumentReference());
+        DocumentReference docRef = attachRef.getDocumentReference();
 
         // Check whether current user has view rights on the document containing the attachment.
-        if (!docBridge.isDocumentViewable(strDocumentName)) {
+        if (!docBridge.isDocumentViewable(docRef)) {
             throw new Exception("Inadequate privileges.");
         }
 
         // If output syntax is not specified, use the default document syntax.
-        String syntaxId = (outputSyntaxId == null) ? docBridge.getDocumentSyntaxId(strDocumentName) : outputSyntaxId;
+        String syntaxId = (outputSyntaxId == null) ? docBridge.getDocument(docRef).getSyntaxId() : outputSyntaxId;
 
         XDOM preview;
         if (isPresentation(attachRef.getName())) {
